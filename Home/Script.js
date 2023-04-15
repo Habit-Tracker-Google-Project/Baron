8a9504653f6cb7da430a8daf89bb38bd29a826c6
Baron Ng <baronng25@gmail.com>
2023-04-14 21:41:45 -0400

    set up some stuff and reorganized css

---
+++ Home/Script.js
@@ -0,0 +1,14 @@
+import { CircularGauge } from '@syncfusion/ej2-circulargauge';
+
+// initialize CircularGauge component
+let gauge = new CircularGauge();
+
+// render initialized CircularGauge
+gauge.appendTo('#container');
+
+
+
+
+
+
+
