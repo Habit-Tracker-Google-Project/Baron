0baab98f22369f64348229539a7e3f58e7f79aa6
Baron Mac <baronng25@gmail.com>
2023-06-22 17:00:29 -0400

    tried fixing scrolling text

--- test_extensions/ScrollingTips.js
+++ test_extensions/ScrollingTips.js
@@ -25,7 +25,6 @@ function randomizeTip() { // will cycle the tips messages
       "Keep your phone out of sight and out of reach!",
       "Manage your email so that you aren't wasting hours going through it!",
       "Listening to music can make the task more enjoyable!",
-
       "It takes around 2 months to make a habit and up to 2 months to break one, keep up the commitment!",
       "The best time to plant a tree was 20 years ago. The second best time is now.",
       "Learn as if you will live forever, live like you will die tomorrow.",
@@ -44,7 +43,7 @@ function randomizeTip() { // will cycle the tips messages
     var text = document.getElementById("scroll-text");
     let instanceCounter = 0;
     var timer = setInterval(function() {
-      if (instanceCounter==6) {
+      if (instanceCounter==2) {
         // every 100s, text will appear and scroll for 20s
         text.innerHTML = tips[Math.floor(Math.random() * tips.length)];
         instanceCounter = 0;
